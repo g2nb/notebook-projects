3599c743734247df740fd8a7ad978e4412627493
tmtabor <tmtabor@cloud.ucsd.edu>
2023-03-08 11:05:34 -0800

    Parse usage event stats after load (still to display the results)

--- static/js/stats.js
+++ static/js/stats.js
@@ -2,7 +2,8 @@ var GenePattern = GenePattern || {};
 GenePattern.stats = GenePattern.stats || {};
 GenePattern.stats.project_updates = GenePattern.stats.project_updates || {};
 GenePattern.stats.project_runs = GenePattern.stats.project_runs || {};
-GenePattern.stats.usage_events = GenePattern.stats.usage_events || {};
+GenePattern.stats.usage_events = GenePattern.stats.usage_events || [];
+GenePattern.stats.event_stats = GenePattern.stats.event_stats || {};
 
 class Stats {
     constructor() {
@@ -21,15 +22,100 @@ class Stats {
     }
 
     init_usage_tab() {
-        Stats.query_usage_events().then(() => Stats.draw_events_table());
+        Stats.query_usage_events().then(() => Stats.compile_usage_stats().then(() => Stats.draw_events_table()));
     }
 
     static query_usage_events() {
-        return fetch(`/services/usage/report/`)
+        return fetch(`https://workspace.g2nb.org/services/usage/report/`)
             .then(response => response.json())
             .then(response => {
                 GenePattern.stats.usage_events = response['events'];
             });
+    }
+
+    static compile_usage_stats() {
+        return new Promise((resolve, reject) => {
+            GenePattern.stats.usage_events.forEach(e => {
+                if (e.event_token === '') e.event_token = 'unknown'; // Special case for unknown tokens
+
+                // Lazily initialize event_stats and increment counts for each event_token
+                if (e.event_token in GenePattern.stats.event_stats) GenePattern.stats.event_stats[e.event_token].count++;
+                else GenePattern.stats.event_stats[e.event_token] = {count: 1};
+
+                // Note the latest time the event happened
+                const created = new Date(e.created);
+                if (!GenePattern.stats.event_stats[e.event_token].latest)
+                    GenePattern.stats.event_stats[e.event_token].latest = created;
+                else if (GenePattern.stats.event_stats[e.event_token].latest < created)
+                    GenePattern.stats.event_stats[e.event_token].latest = created;
+
+                // Special parsing for tool_run
+                if (e.event_token === 'tool_run') {
+                    // Lazily initialize the list of origins
+                    if (!GenePattern.stats.event_stats[e.event_token].origins)
+                        GenePattern.stats.event_stats[e.event_token].origins = {};
+
+                    // Parse the description and handle blanks
+                    let [origin, id, tool] = e.description.split('|');
+                    if (!origin) origin = 'unknown';
+                    if (!tool) tool = 'unknown';
+
+                    // Lazily initialize the specific origin
+                    if (!GenePattern.stats.event_stats[e.event_token].origins[origin])
+                        GenePattern.stats.event_stats[e.event_token].origins[origin] = {};
+
+                    // Lazily initialize the specific tool or increment the count
+                    if (!GenePattern.stats.event_stats[e.event_token].origins[origin][tool])
+                        GenePattern.stats.event_stats[e.event_token].origins[origin][tool] = {count: 1}
+                    else GenePattern.stats.event_stats[e.event_token].origins[origin][tool].count++;
+                }
+
+                // Special parsing for project_launch
+                else if (e.event_token === 'project_launch') {
+                    // Lazily initialize the list of users
+                    if (!GenePattern.stats.event_stats[e.event_token].users)
+                        GenePattern.stats.event_stats[e.event_token].users = {};
+
+                    // Parse the description
+                    const [user, project] = e.description.split('|');
+
+                    // Lazily initialize the specific user
+                    if (!GenePattern.stats.event_stats[e.event_token].users[user])
+                        GenePattern.stats.event_stats[e.event_token].users[user] = {};
+
+                    // Lazily initialize the specific project or increment the count
+                    if (!GenePattern.stats.event_stats[e.event_token].users[user][project])
+                        GenePattern.stats.event_stats[e.event_token].users[user][project] = {count: 1}
+                    else GenePattern.stats.event_stats[e.event_token].users[user][project].count++;
+                }
+
+                // Special parsing for labextension_load
+                else if (e.event_token === 'labextension_load') {
+                    // Lazily initialize the list of domains
+                    if (!GenePattern.stats.event_stats[e.event_token].domains)
+                        GenePattern.stats.event_stats[e.event_token].domains = {};
+
+                    // Parse the description
+                    const url = new URL(e.description)
+
+                    // Lazily initialize the specific domain or increment the count
+                    if (!GenePattern.stats.event_stats[e.event_token].domains[url.hostname])
+                        GenePattern.stats.event_stats[e.event_token].domains[url.hostname] = {count: 1};
+                    else GenePattern.stats.event_stats[e.event_token].domains[url.hostname].count++;
+                }
+
+                // Handle other event_tokens
+                else {
+                    // Lazily initialize the list of descriptions
+                    if (!GenePattern.stats.event_stats[e.event_token].descriptions)
+                        GenePattern.stats.event_stats[e.event_token].descriptions = [];
+                    // Append to the list
+                    GenePattern.stats.event_stats[e.event_token].descriptions.push(e.description);
+                }
+            });
+
+            resolve();
+        });
     }
 
     static draw_project_tables() {
